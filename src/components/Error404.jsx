553a982cdb72d11e15d35564f83a7772e8e469d0
Jonathan Weiss <jonathan.weiss@domain.com.au>
2018-08-23 08:47:07 +1000

    Fix 404 image

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -4,7 +4,7 @@ const Error404 = () => (
   <div className="columns">
     <div className="column col-12">
       <h1>404 - page not found</h1>
-      <img alt="Sad kitten" src="http://i94.photobucket.com/albums/l92/Ruas_2006/Caturday/Sad_Kitten_Is_Sad.jpg" />
+      <img alt="Sad kitten" src="https://www.catster.com/wp-content/uploads/2017/12/Sad-young-brown-tabby-kitten.jpg" />
     </div>
   </div>
 );
